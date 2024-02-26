16b944b59f0cbdf835072fd01765ea1843451d19
JarnoDeSmedt <jarno.desmedt@live.be>
2024-02-26 15:36:47 +0100

    fix duration metrics and chart legend dashboard

--- pages/Dashboard.py
+++ pages/Dashboard.py
@@ -28,6 +28,8 @@ elif st.session_state["authentication_status"] == None:
 elif st.session_state["authentication_status"]:
     st.subheader(f'Welcome to your archery dashboard *{st.session_state["name"]}*!')
     
+    st.markdown("---")
+    
     # Create a 2x2 layout for the duration metrics
     col1, col2 = st.columns(2)
     
@@ -70,91 +72,121 @@ elif st.session_state["authentication_status"]:
 
     
     # --- KPI - total cardio this week
-    
-    cardio = data[['Date','Cardio hh:mm']]
-    
+
+    cardio = data[['Date', 'Cardio hh:mm']]
+
     # Convert 'Date' to datetime
     cardio['Date'] = pd.to_datetime(cardio['Date'])
     cardio['Cardio hh:mm'] = pd.to_timedelta(cardio['Cardio hh:mm'])
-    
+
     # Extract week information from the date column
     cardio['week'] = cardio['Date'].dt.isocalendar().week
-    
+
     # Sum the time spent per week
     total_time_cardio_per_week = cardio.groupby('week')['Cardio hh:mm'].sum()
-    
-    # Convert timedelta to hours and minutes
-    total_time_per_week_hours = total_time_cardio_per_week.dt.total_seconds() / 3600
-    total_time_per_week_str = total_time_per_week_hours.apply(lambda x: f"{int(x)}h {int((x % 1) * 60)}m")
+
+    # Sum the total time in seconds
+    total_seconds = total_time_cardio_per_week.dt.total_seconds().sum()
+
+    # Convert total seconds to hours and minutes
+    total_hours = int(total_seconds / 3600)
+    total_minutes = int((total_seconds % 3600) / 60)
+
+    # Format the total time as a string
+    total_time_str = f"{total_hours}h {total_minutes}m"
 
     # Display the result using st.metric
-    col1.metric(label='Total Time Cardio per Week', value=total_time_per_week_str.sum())
+    col1.metric(label='Total Time Cardio per Week', value=total_time_str)
+
     
 
     # --- KPI - total gym this week 
-    
-    gym = data[['Date','Gym hh:mm']]
-    
+
+    gym = data[['Date', 'Gym hh:mm']]
+
     # Convert 'Date' to datetime
     gym['Date'] = pd.to_datetime(gym['Date'])
     gym['Gym hh:mm'] = pd.to_timedelta(gym['Gym hh:mm'])
-    
+
     # Extract week information from the date column
     gym['week'] = gym['Date'].dt.isocalendar().week
-    
+
     # Sum the time spent per week
     total_time_gym_per_week = gym.groupby('week')['Gym hh:mm'].sum()
-    
-    # Convert timedelta to hours and minutes
-    total_time_per_week_hours = total_time_gym_per_week.dt.total_seconds() / 3600
-    total_time_per_week_str = total_time_per_week_hours.apply(lambda x: f"{int(x)}h {int((x % 1) * 60)}m")
+
+    # Sum the total time in seconds
+    total_seconds_gym = total_time_gym_per_week.dt.total_seconds().sum()
+
+    # Convert total seconds to hours and minutes
+    total_hours_gym = int(total_seconds_gym / 3600)
+    total_minutes_gym = int((total_seconds_gym % 3600) / 60)
+
+    # Format the total time as a string
+    total_time_str_gym = f"{total_hours_gym}h {total_minutes_gym}m"
 
     # Display the result using st.metric
-    col1.metric(label='Total Time Gym per Week', value=total_time_per_week_str.sum())
+    col1.metric(label='Total Time Gym per Week', value=total_time_str_gym)  ###
+
     
     # --- KPI - total static this week
-    
-    static = data[['Date','Static Work hh:mm']]
-    
+
+    static = data[['Date', 'Static Work hh:mm']]
+
     # Convert 'Date' to datetime
     static['Date'] = pd.to_datetime(static['Date'])
     static['Static Work hh:mm'] = pd.to_timedelta(static['Static Work hh:mm'])
-    
+
     # Extract week information from the date column
     static['week'] = static['Date'].dt.isocalendar().week
-    
+
     # Sum the time spent per week
     total_time_static_per_week = static.groupby('week')['Static Work hh:mm'].sum()
-    
-    # Convert timedelta to hours and minutes
-    total_time_per_week_hours = total_time_static_per_week.dt.total_seconds() / 3600
-    total_time_per_week_str = total_time_per_week_hours.apply(lambda x: f"{int(x)}h {int((x % 1) * 60)}m")
+
+    # Sum the total time in seconds
+    total_seconds_static = total_time_static_per_week.dt.total_seconds().sum()
+
+    # Convert total seconds to hours and minutes
+    total_hours_static = int(total_seconds_static / 3600)
+    total_minutes_static = int((total_seconds_static % 3600) / 60)
+
+    # Format the total time as a string
+    total_time_str_static = f"{total_hours_static}h {total_minutes_static}m"
 
     # Display the result using st.metric
-    col2.metric(label='Total Static Work per Week', value=total_time_per_week_str.sum())
+    col2.metric(label='Total Static Work per Week', value=total_time_str_static)  ###
+
     
     # --- KPI - total yoga this week
-    
-    yoga = data[['Date','Yoga hh:mm']]
-    
+
+    yoga = data[['Date', 'Yoga hh:mm']]
+
     # Convert 'Date' to datetime
     yoga['Date'] = pd.to_datetime(yoga['Date'])
     yoga['Yoga hh:mm'] = pd.to_timedelta(yoga['Yoga hh:mm'])
-    
+
     # Extract week information from the date column
     yoga['week'] = yoga['Date'].dt.isocalendar().week
-    
+
     # Sum the time spent per week
     total_time_yoga_per_week = yoga.groupby('week')['Yoga hh:mm'].sum()
-    
-    # Convert timedelta to hours and minutes
-    total_time_per_week_hours = total_time_yoga_per_week.dt.total_seconds() / 3600
-    total_time_per_week_str = total_time_per_week_hours.apply(lambda x: f"{int(x)}h {int((x % 1) * 60)}m")
+
+    # Sum the total time in seconds
+    total_seconds_yoga = total_time_yoga_per_week.dt.total_seconds().sum()
+
+    # Convert total seconds to hours and minutes
+    total_hours_yoga = int(total_seconds_yoga / 3600)
+    total_minutes_yoga = int((total_seconds_yoga % 3600) / 60)
+
+    # Format the total time as a string
+    total_time_str_yoga = f"{total_hours_yoga}h {total_minutes_yoga}m"
 
     # Display the result using st.metric
-    col2.metric(label='Total Yoga per Week', value=total_time_per_week_str.sum())
+    col2.metric(label='Total Yoga per Week', value=total_time_str_yoga)  ###
+
     
     st.markdown("""---""")
+    
+    st.subheader("Periodical Summary")
     
     st.write("Make selections in the left sidebar to update")
     
@@ -175,7 +207,7 @@ elif st.session_state["authentication_status"]:
 
     # Display the total arrows metric for the selected date range
     total_arrows_for_selected_range = int(filtered_data['Total Arrows'].sum())
-    st.metric(label='Total Arrows uit selectie', value=total_arrows_for_selected_range)
+    st.metric(label='Total Arrows from date range', value=total_arrows_for_selected_range)
     
     
     # TODO chart number of arrows per month/year,... plotlly interactive
@@ -184,22 +216,31 @@ elif st.session_state["authentication_status"]:
     # --- PLOT
     
     # Convert 'Date' to datetime
-    data['Date'] = pd.to_datetime(data['Date'])
+    arrowplotdata = data.drop(columns=['Time hh:mm', 'Special commentary', 'Gym hh:mm', 'Yoga hh:mm','Cardio hh:mm', 'Static Work hh:mm'])
+    
+    arrowplotdata['Date'] = pd.to_datetime(data['Date'])
 
     # Melt the DataFrame to have a 'Distance' column and a 'Value' column
-    melted_data = pd.melt(data, id_vars=['Date'], var_name='Distance', value_name='Value')
+    melted_data = pd.melt(arrowplotdata, id_vars=['Date'], var_name='Distance', value_name='Value')
     
     # Exclude rows where 'Distance' is 'Total Arrows'
     melted_data = melted_data[melted_data['Distance'] != 'Total Arrows']
 
     # Create a stacked bar chart
     fig = px.bar(melted_data, x='Date', y='Value', color='Distance',
-                title='Arrows Shot per Day for Each Distance',
                 labels={'Value': 'Arrows Shot', 'Distance': 'Distance'},
                 category_orders={'Distance': sorted(data.columns[1:])})
-
+    
+    st.write("Arrows Shot per Day for Each Distance")
+    
+    fig.update_layout(margin=dict(t=100))
+    
     # Show the legend
-    fig.update_layout(showlegend=True)
+    fig.update_layout(legend=dict(orientation="h", yanchor="bottom", xanchor="center", y=1.02, x=0.5, font=dict(size=20)), showlegend=True)
+    
+    # Add space to the right of the legend
+    #fig.update_layout(legend=dict(x=0.6, y=1.15))
+
     
     # Disable zooming and panning
     fig.update_layout(dragmode='pan')
