c86f92df8d5819b58069ed916a53bc701eabd595
Jarno De Smedt <jarno.desmedt@live.be>
2024-12-23 20:37:15 +0100

    refactor

--- pages/Upload-sqlite.py
+++ pages/Upload-sqlite.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import os
 # display title
 st.title(f"Upload a sqlite file!")
 
@@ -7,11 +8,13 @@ if not st.session_state['authentication_status']:
     st.stop()  # Do not continue if check_password is not True.
 
 st.text("upload your sqlite file")
-uploaded_sqlite_file = st.file_uploader("Choose a file")
+uploaded_sqlite_file = st.file_uploader("Choose a file", type=["sqlite", "db"])
 
 if uploaded_sqlite_file is not None:
-    # Save the uploaded file to the current directory
-    with open(uploaded_sqlite_file.name, "wb") as f:
+    # Save the uploaded file to the DATA directory
+    file_path = os.path.join('./DATA/', uploaded_sqlite_file.name)
+    with open(file_path, "wb") as f:
         f.write(uploaded_sqlite_file.getbuffer())
 
     st.text("File uploaded")
+    st.text(f"Saved to {file_path}")
